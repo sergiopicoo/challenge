835d0b622f0ae625d695d0a83776f2ffb526a960
sergiopicoo <vazquezs@umsystem.edu>
2023-03-10 15:37:28 -0600

    1st commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,18 @@
 ## test
+
+for integer in range(1,101):
+    st =""
+
+    if integer% 3 == 0:
+        st = st + "Fizz"
+
+    elif integer% 5 == 0:
+        st = st + "Buzz"
+
+    elif integer% 5 != 0 and integer% 3 != 0:
+        st = st + str(integer)
+
+    print(st)
+
+
+
