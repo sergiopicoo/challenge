de3f8b6b0c0dc8d0378a67da6b1346167c5cf154
sergiopicoo <92826319+sergiopicoo@users.noreply.github.com>
2023-03-10 16:01:45 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,10 +2,10 @@ for integer in range(1,100):
   
     string =""
     
-    if integer% 3 == 0:
-        string = string + "Fizz"
-    elif integer% 5 == 0:
+    if integer% 5 == 0:
         string = string + "Buzz"
+    elif integer% 3 == 0:
+        string = string + "Fizz"
     elif integer% 5 != 0 and integer% 3 != 0:
         string = string + str(integer)
 
