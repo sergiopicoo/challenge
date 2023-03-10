fc1fe4fc8770508ae728301d18986304a4123e24
sergiopicoo <92826319+sergiopicoo@users.noreply.github.com>
2023-03-10 15:57:44 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,12 @@
-## test
+for integer in range(1,100):
+  
+    string =""
+    
+    if integer% 3 == 0:
+        string = string + "Fizz"
+    elif integer% 5 == 0:
+        string = string + "Buzz"
+    elif integer% 5 != 0 and integer% 3 != 0:
+        string = string + str(integer)
+
+    print(string)
