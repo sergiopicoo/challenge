d630a90aeeb9e0e711e20dfe0504fe44d51053a7
sergiopicoo <92826319+sergiopicoo@users.noreply.github.com>
2023-03-10 19:57:29 -0600

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,15 @@
-## test
-
 for integer in range(1,100):
     st =""
 
     if integer% 3 == 0:
-        st = st + "Fizz"
-
+        print("Fizz")
+       
     elif integer% 5 == 0:
-        st = st + "Buzz"
+        print("Buzz")
 
     elif integer% 5 != 0 and integer% 3 != 0:
         st = st + str(integer)
 
-    print(st)
-
+    print(st) 
 
 
